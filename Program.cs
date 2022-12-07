09012d0522d275089a7b0e62cd8fd81d859fa653
zarina <zarina100685@mail.ru>
2022-12-07 05:35:42 +0300

    работа выполнена

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// Домашняя работа 1.
+
+// Задача 1. Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+/*
+
+
+Console.Write("Input a  number1: "); 
+int num1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input a  number2: " );
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1<num2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+   Console.WriteLine("No"); 
+}
+*/
+
+
+// Задача 2. Напишите программу которая, принимает на вход три числа и выдает максимальное из этих чисел.
+/*
+Console.Write("Input a first number:" );
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number:" );
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number:" );
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if ( a <  b ) max = b;
+if ( a < c )  max = c;
+
+
+Console.WriteLine( max );
+*/
+// Задача 3. Напишите программу которая, на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка.)
+/*
+
+Console.Write("Input a first a:" );
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a % 2 == 0 )
+{
+   Console.WriteLine("even number" );
+}
+else 
+{
+   Console.WriteLine("odd number" );
+}
+*/
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
